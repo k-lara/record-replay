aba95e83d440d8e9f704ff3f2e42ff34d7c11010
Klara <klara.brandstatter@inext.at>
2024-05-14 10:05:06 +0100

    finish motion replayer side, start, stop, loading, and deleting a replay

--- Assets/Scripts/Replayable.cs
+++ Assets/Scripts/Replayable.cs
@@ -8,17 +8,23 @@ using Ubiq.Messaging;
 using Ubiq.Spawning;
 using UnityEngine;
 
+/// <summary>
+/// a game object that has Replayable might have only been created for replaying
+/// we don't need to know when we were created and when we get deleted, because the Replayer will handle this
+/// we only need to know when to start and stop replaying the data we got sent from the Replayer
+/// </summary>
 public class Replayable : MonoBehaviour
 {
     private Replayer _replayer;
     private Recordable.RecordableData _replayableData;
-    private bool _isReplaying;
+    private bool _isPlaying; // true if a replay is playing
     
     private ThreePointTrackedAvatar _trackedAvatar;
     private int _trackingPoints;
     private int _fps;
     private int _frames;
-    private float _replayStartTime;
+    private State[] _state; // current pose of the tracked avatar
+    private float _deltaTime; // time that has progressed since the replay started
     
     // taken from ThreePointTrackedAvatar to create the same reference counted scene graph message
     [Serializable]
@@ -31,8 +37,7 @@ public class Replayable : MonoBehaviour
         public float rightGrip;
     }
     
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _replayer = GameObject.FindWithTag("Recorder").GetComponent<Replayer>();
         _replayer.onReplayStart.AddListener(OnReplayStart);
@@ -42,15 +47,22 @@ public class Replayable : MonoBehaviour
         
     }
     
+    // in theory, whenever someone presses start again, the replay starts from the beginning
+    // only when it is on pause and the user presses start again, it should continue from where it was paused
     private void OnReplayStart()
     {
-        _isReplaying = true;
-        _replayStartTime = Time.time;
+        // if we have stopped a replay pressing start will resume it
+        // in case we are already playing pressing start will start the replay from the beginning
+        if (_isPlaying)
+            _deltaTime = 0.0f;
+        
+        _isPlaying = true;
     }
-    
+    // stop a replay without deleting the replay
+    // this is only invoked from Replayer when the replay is already playing otherwise it is unnecessary
     private void OnReplayStop()
     {
-        _isReplaying = false;
+        _isPlaying = false;
     }
 
     public void SetReplayableData(Recordable.RecordableData data)
@@ -60,52 +72,34 @@ public class Replayable : MonoBehaviour
         _trackingPoints = int.Parse(_replayableData.metaData[_replayableData.metaDataLabels.IndexOf("trackingPoints")]);
         _fps = int.Parse(_replayableData.metaData[_replayableData.metaDataLabels.IndexOf("fps")]);
         _frames = int.Parse(_replayableData.metaData[_replayableData.metaDataLabels.IndexOf("frames")]);
-    }
-
-    private ReferenceCountedSceneGraphMessage CreateRCSGMessage(State[] state)
-    {
-        var transformBytes = MemoryMarshal.AsBytes(new ReadOnlySpan<State>(state));
-        var message = ReferenceCountedSceneGraphMessage.Rent(transformBytes.Length);
-        transformBytes.CopyTo(new Span<byte>(message.bytes, message.start, message.length));
-        return message;
+        
+        //put prefab in correct first pose for replay
+        SetPoseFrame(0);
     }
     
-    // Update is called once per frame
-    void Update()
+    // set the pose of the tracked avatar to the pose of the frame
+    // when loading a recording we set the pose of the recorded avatars to the first frame
+    private void SetPoseFrame(int frame)
     {
-        if (!_isReplaying) return;
+        var firstDataFrame = _replayableData.recordableData[0];
         
-        // we have _frames in total which were recorded at _fps
-        // during replay we interpolate between the frames to get smooth movements
-        // because we know the fps we always know where we are between two consecutive frames and can interpolate accordingly
-        var t = (Time.time - _replayStartTime) * _fps;
-        var frameNr = Mathf.FloorToInt(t);
-        t -= frameNr;
+        var pHead = new Vector3(firstDataFrame.data[0], firstDataFrame.data[1], firstDataFrame.data[2]);
+        var pLeftHand = new Vector3(firstDataFrame.data[3], firstDataFrame.data[4], firstDataFrame.data[5]);
+        var pRightHand = new Vector3(firstDataFrame.data[6], firstDataFrame.data[7], firstDataFrame.data[8]);
+        
+        var rHead = new Quaternion(firstDataFrame.data[9], firstDataFrame.data[10], firstDataFrame.data[11], firstDataFrame.data[12]);
+        var rLeftHand = new Quaternion(firstDataFrame.data[13], firstDataFrame.data[14], firstDataFrame.data[15], firstDataFrame.data[16]);
+        var rRightHand = new Quaternion(firstDataFrame.data[17], firstDataFrame.data[18], firstDataFrame.data[19], firstDataFrame.data[20]);
 
-        if (frameNr < _frames - 1)
-        {
-            // Interpolate the pose between two frames in the recordable data and send it to the tracked avatar
-            var state = InterpolatePose(_replayableData.recordableData[frameNr], _replayableData.recordableData[frameNr + 1], t);
-            _trackedAvatar.ProcessMessage(CreateRCSGMessage(state));
-        }
-        else
-        {
-            // end of replay reached, start again
-            _replayStartTime = Time.time;
-        }
+        _state = GetStateFrom(pHead, pLeftHand, pRightHand, rHead, rLeftHand, rRightHand);
+        // we can't set the pose here because ThreePointTrackedAvatar has not initialised the networkSceneRoot yet
+        // but we set it in update anyway so don't need to do anything here
+        
     }
-
-    private State[] InterpolatePose(Recordable.RecordableDataFrame f1, Recordable.RecordableDataFrame f2, float t)
+    
+    private State[] GetStateFrom(Vector3 pHead, Vector3 pLeftHand, Vector3 pRightHand, Quaternion rHead, Quaternion rLeftHand, Quaternion rRightHand)
     {
         var state = new State[1];
-        
-        var pHead = Vector3.Lerp(new Vector3(f1.data[0], f1.data[1], f1.data[2]), new Vector3(f2.data[0], f2.data[1], f2.data[2]), t);
-        var pLeftHand = Vector3.Lerp(new Vector3(f1.data[3], f1.data[4], f1.data[5]), new Vector3(f2.data[3], f2.data[4], f2.data[5]), t);
-        var pRightHand = Vector3.Lerp(new Vector3(f1.data[6], f1.data[7], f1.data[8]), new Vector3(f2.data[6], f2.data[7], f2.data[8]), t);
-       
-        var rHead = Quaternion.Lerp(new Quaternion(f1.data[9], f1.data[10], f1.data[11], f1.data[12]), new Quaternion(f2.data[9], f2.data[10], f2.data[11], f2.data[12]), t);
-        var rLeftHand = Quaternion.Lerp(new Quaternion(f1.data[13], f1.data[14], f1.data[15], f1.data[16]), new Quaternion(f2.data[13], f2.data[14], f2.data[15], f2.data[16]), t);
-        var rRightHand = Quaternion.Lerp(new Quaternion(f1.data[17], f1.data[18], f1.data[19], f1.data[20]), new Quaternion(f2.data[17], f2.data[18], f2.data[19], f2.data[20]), t);
         
         state[0].head = new PositionRotation
         {
@@ -126,6 +120,66 @@ public class Replayable : MonoBehaviour
         state[0].rightGrip = 0;
         
         return state;
+    }
+
+    private ReferenceCountedSceneGraphMessage CreateRcsgMessage(State[] state)
+    {
+        var transformBytes = MemoryMarshal.AsBytes(new ReadOnlySpan<State>(state));
+        var message = ReferenceCountedSceneGraphMessage.Rent(transformBytes.Length);
+        transformBytes.CopyTo(new Span<byte>(message.bytes, message.start, message.length));
+        return message;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isPlaying)
+        {
+            _deltaTime += Time.deltaTime;
+            // we have _frames in total which were recorded at _fps
+            // during replay we interpolate between the frames to get smooth movements
+            // because we know the fps we always know where we are between two consecutive frames and can interpolate accordingly
+            var t = _deltaTime * _fps;
+            var frameNr = Mathf.FloorToInt(t);
+            t -= frameNr;
+
+            if (frameNr < _frames - 1)
+            {
+                // Interpolate the pose between two frames in the recordable data and send it to the tracked avatar
+                _state = InterpolatePose(_replayableData.recordableData[frameNr], _replayableData.recordableData[frameNr + 1], t);
+                _trackedAvatar.ProcessMessage(CreateRcsgMessage(_state));
+            }
+            else
+            {
+                // end of replay reached, start again
+                _deltaTime = 0.0f;
+            }
+        }
+        else
+        {
+            if (_state != null)
+            {
+                // we are not playing, but we have a state, so we are paused
+                // send the last state to the tracked avatar
+                _trackedAvatar.ProcessMessage(CreateRcsgMessage(_state));
+            }
+        }
+        
+    }
+
+    private State[] InterpolatePose(Recordable.RecordableDataFrame f1, Recordable.RecordableDataFrame f2, float t)
+    {
+        var state = new State[1];
+        
+        var pHead = Vector3.Lerp(new Vector3(f1.data[0], f1.data[1], f1.data[2]), new Vector3(f2.data[0], f2.data[1], f2.data[2]), t);
+        var pLeftHand = Vector3.Lerp(new Vector3(f1.data[3], f1.data[4], f1.data[5]), new Vector3(f2.data[3], f2.data[4], f2.data[5]), t);
+        var pRightHand = Vector3.Lerp(new Vector3(f1.data[6], f1.data[7], f1.data[8]), new Vector3(f2.data[6], f2.data[7], f2.data[8]), t);
+       
+        var rHead = Quaternion.Lerp(new Quaternion(f1.data[9], f1.data[10], f1.data[11], f1.data[12]), new Quaternion(f2.data[9], f2.data[10], f2.data[11], f2.data[12]), t);
+        var rLeftHand = Quaternion.Lerp(new Quaternion(f1.data[13], f1.data[14], f1.data[15], f1.data[16]), new Quaternion(f2.data[13], f2.data[14], f2.data[15], f2.data[16]), t);
+        var rRightHand = Quaternion.Lerp(new Quaternion(f1.data[17], f1.data[18], f1.data[19], f1.data[20]), new Quaternion(f2.data[17], f2.data[18], f2.data[19], f2.data[20]), t);
+        
+        return GetStateFrom(pHead, pLeftHand, pRightHand, rHead, rLeftHand, rRightHand);
     }
     
     private void OnDestroy()
--- Assets/Scripts/Replayer.cs
+++ Assets/Scripts/Replayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using Ubiq.Geometry;
 using Ubiq.Spawning;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -13,15 +14,17 @@ public class Replayer : MonoBehaviour
     private string _filePath; // file path to the saved data of the last recording
     private List<Recordable.RecordableData> _recordableDataList;
     private string _savePath; // get it from recorder
+    private bool _isLoaded;
+    private bool _isPlaying;
     
     private NetworkSpawnManager _spawnManager;
     private Dictionary<string, GameObject> _prefabCatalogue; 
     private List<GameObject> _spawnedObjects = new List<GameObject>();
     
-    public UnityEvent onReplayStart;
-    public UnityEvent onReplayStop;
-    public UnityEvent onReplayCreated;
-    public UnityEvent onReplayDeleted;
+    public UnityEvent onReplayCreated; // replay created
+    public UnityEvent onReplayStart; // start replaying
+    public UnityEvent onReplayStop; // stop replaying
+    public UnityEvent onReplayDeleted; // replay deleted
     
     // Start is called before the first frame update
     void Start()
@@ -39,25 +42,47 @@ public class Replayer : MonoBehaviour
         }
             
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
+    // a replay has to be loaded for this to work
     public void StartReplay()
     {
+        if (!_isLoaded) return;
+        Debug.Log("Start replay");
+        _isPlaying = true;
         onReplayStart.Invoke();
     }
     
+    // a replay has to be loaded and playing for this to work
     public void StopReplay()
     {
+        if (!_isLoaded || !_isPlaying) return;
+        Debug.Log("Stop replay!");
+        _isPlaying = false;
         onReplayStop.Invoke();
+    }
+    
+    // a replay has to be loaded, can be playing or paused
+    public void DeleteReplay()
+    {
+        StopReplay();
+        onReplayDeleted.Invoke();
+        CleanupReplay();
+        _isLoaded = false;
+        Debug.Log("Replay deleted!");
+    }
+
+    private void CleanupReplay()
+    {
+        for(var i = 0; i < _spawnedObjects.Count; i++)
+        {
+            _spawnManager.Despawn(_spawnedObjects[i]);
+        }
+        _spawnedObjects.Clear();
     }
     
     public void LoadReplay(string filePath = null)
     {   
+        Debug.Log( filePath == null ? "Load last recording!" : "Load recording from" + filePath);
         // if no file path is given we do not load a recording from file but use the data from the last recording
         if (filePath == null)
         {
@@ -67,16 +92,27 @@ public class Replayer : MonoBehaviour
         {
             StartCoroutine(LoadRecordedDataFromFile(filePath));
         }
+        onReplayCreated.Invoke();
+        _isLoaded = true;
     }
-
+    
+    // when we are recording a previous replay and the recording stops we automatically delete the current replay
+    // this gets called right after a save file is created but before the newly recorded data is written to the file
+    // once the data is written to file, the new recording is loaded as the new replay
+    // this should be fine as we have already deleted the old replay
     private void OnRecordingStop()
     {
         _filePath = _recorder.GetPathLastRecording();
+        if (_isPlaying)
+        {
+            DeleteReplay();
+        }
     }
     
     private void OnRecordingSaved(List<Recordable.RecordableData> recordableDataList)
     {
         _recordableDataList = recordableDataList;
+        LoadReplay();
     }
 
     public void CreateReplay()
@@ -84,17 +120,21 @@ public class Replayer : MonoBehaviour
         foreach (var record in _recordableDataList)
         {
             // spawn prefab based on name stored in recordable data
+            // this does not work because the name is the network id of the avatar not the prefab name
             var prefabName = record.metaData[record.metaDataLabels.IndexOf("prefab")];
             var go = _spawnManager.SpawnWithPeerScope(_prefabCatalogue[prefabName]);
             _spawnedObjects.Add(go);
             
             var replayable = go.GetComponent<Replayable>();
             replayable.SetReplayableData(record);
+            
         }
+        Debug.Log("Replay created!");
     }
 
     private IEnumerator LoadRecordedDataFromFile(string filePath)
     {
+        Debug.Log("Load replay from file!");
         using StreamReader sr = new StreamReader(Path.Join(_savePath, filePath));
         
         string recordableDataJson;
@@ -107,9 +147,28 @@ public class Replayer : MonoBehaviour
         CreateReplay();
         yield return null;
     }
-    
-    private void Replay()
+}
+[CustomEditor(typeof(Replayer))]
+public class ReplayerEditor : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
     {
+        DrawDefaultInspector();
+        if (!Application.isPlaying) return;
         
+        Replayer replayer = (Replayer)target;
+        
+        if (GUILayout.Button("Start Replay"))
+        {
+            replayer.StartReplay();
+        }
+        if (GUILayout.Button("Stop Replay"))
+        {
+            replayer.StopReplay();
+        }
+        if (GUILayout.Button("Delete Replay"))
+        {
+            replayer.DeleteReplay();
+        }
     }
 }
