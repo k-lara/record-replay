28d1f30cb97468561803d5a16296767f68ebdfda
Klara <klara.brandstatter@inext.at>
2024-06-07 17:18:49 +0100

    vr button trigger for recorder UI

---
+++ Assets/VRButtonTrigger.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VRButtonTrigger : MonoBehaviour
+{
+    public float deadTime = 3.0f;
+
+    private bool _deadTimeActive = false;
+
+    public UnityEvent onPressed, onReleased;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Button") && !_deadTimeActive)
+        {
+            onPressed.Invoke();
+            Debug.Log("Button pressed");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Button") && !_deadTimeActive)
+        {
+            onReleased.Invoke();
+            Debug.Log("Button released");
+            StartCoroutine(WaitForDeadTime());
+        }
+    }
+
+    IEnumerator WaitForDeadTime()
+    {
+        _deadTimeActive = true;
+        yield return new WaitForSeconds(deadTime);
+        _deadTimeActive = false;
+    }
+}
