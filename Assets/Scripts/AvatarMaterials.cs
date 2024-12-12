9df54e9cf3e0c335e01fffdc4953f7417adaf9be
Klara <klara.brandstatter@inext.at>
2024-12-12 21:30:03 +0000

    update Avatarmaterials

--- Assets/Scripts/AvatarMaterials.cs
+++ Assets/Scripts/AvatarMaterials.cs
@@ -16,9 +16,36 @@ public class AvatarMaterials : MonoBehaviour
         }
     }
     
-    [Tooltip("All opaque materials that are used on this avatar. Should be in the same order as the the fade materials.")]
+    // get materials by name
+    public Material GetMaterialOpaque(string name)
+    {
+        foreach (var material in materialsOpaque)
+        {
+            // Debug.Log(material.name + " " + name);
+            if (material.name == name)
+            {
+                return material;
+            }
+        }
+        return null;
+    }
+    
+    public Material GetMaterialFade(string name)
+    {
+        foreach (var material in materialsFade)
+        {
+            // Debug.Log(material.name + " " + name);
+            if (material.name == name)
+            {
+                return material;
+            }
+        }
+        return null;
+    }
+    
+    [Tooltip("All opaque materials that are used on this avatar. Should be in the same order as the the fade materials. Make sure names are without whitespace!")]
     public List<Material> materialsOpaque = new();
-    [Tooltip("Should be in the same order as materialsOpaque.")]
+    [Tooltip("Should be in the same order as materialsOpaque. Make sure names are without whitespace!")]
     public List<Material> materialsFade = new();
 
 }
